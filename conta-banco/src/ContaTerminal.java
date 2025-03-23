01c88d1e19fb52bbf1a3a8144fb96266ebc480dd
francofgdev <francofg.dev@gmail.com>
2025-03-22 21:23:02 -0300

    Desafio do módulo Sintaxe Java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número da sua agência: ");
+        String Agencia = sc.nextLine();
+        System.out.println();
+
+        System.out.println("Digite o número da sua conta: ");
+        String numeroContaStr = sc.nextLine();
+        int Numero = Integer.parseInt(numeroContaStr);
+        System.out.println();
+
+        System.out.println("Digite seu nome completo: ");
+        String NomeCliente = sc.nextLine();
+        System.out.println();
+
+        System.out.println("Digite o valor do depósito inicial: ");
+        String SaldoStr = sc.nextLine();
+        double Saldo = Double.parseDouble(SaldoStr);
+
+        sc.close();
+
+        DadosCliente cliente = new DadosCliente(Numero, Agencia, NomeCliente, Saldo);
+
+        System.out.println(cliente.toString());
     }
 }
