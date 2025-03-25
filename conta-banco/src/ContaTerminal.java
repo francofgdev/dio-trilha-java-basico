e6b5391dd0104013725b7bd8daf8f3a1aa36f8ff
francofgdev <francofg.dev@gmail.com>
2025-03-25 20:23:12 -0300

    feature/terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,29 +2,29 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite o número da sua agência: ");
-        String Agencia = sc.nextLine();
-        System.out.println();
+        System.out.println("Por favor, digite o número da Agência: ");
+        int agencia = sc.nextInt();
 
-        System.out.println("Digite o número da sua conta: ");
-        String numeroContaStr = sc.nextLine();
-        int Numero = Integer.parseInt(numeroContaStr);
-        System.out.println();
+        System.out.println("Digite o número da Conta: ");
+         int conta = sc.nextInt();
 
-        System.out.println("Digite seu nome completo: ");
-        String NomeCliente = sc.nextLine();
-        System.out.println();
+         System.out.print("Informe o seu nome : ");
+         String nome = sc.next();
+         sc.nextLine();
 
-        System.out.println("Digite o valor do depósito inicial: ");
-        String SaldoStr = sc.nextLine();
-        double Saldo = Double.parseDouble(SaldoStr);
+         System.out.print("Informe o seu sobrenome : ");
+         String sobrenome = sc.next();
+              
+         System.out.println("Informe o seu saldo inicial: ");
+         Double saldo = sc.nextDouble();
+         
+         System.out.println();
+         System.out.printf("Olá %s %s, obrigado por criar uma conta em nosso banco, sua Agência é %d, Conta número %d e seu saldo de %.2f já está disponível para saque!", nome, sobrenome, agencia, conta, saldo);
 
         sc.close();
 
-        DadosCliente cliente = new DadosCliente(Numero, Agencia, NomeCliente, Saldo);
-
-        System.out.println(cliente.toString());
     }
 }
